d21c6ba6f77f26733c44e66c6fe2542995fa7361
Roberta Brandao <roberta.brandao@gerdau.com.br>
2023-08-28 19:52:51 +0000

    add script to create posts

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+selection=$1
+arg=$2
+
+
+case $selection in
+	new | n)
+	   hugo new posts/${arg}.md
+	   mkdir -p content/posts/${arg}/
+	   mv content/posts/${arg}.md content/posts/${arg}/index.md
+	;;
+
+	serve | s)
+	   hugo --config config.toml serve -D --port 8080 --bind ${arg}
+	;;
+	*)
+	   echo "Usage:"
+	   echo "$0 [new|n] 'article-title-name' - To create new article"
+	   echo "$0 [serve|s]  IP_BIND - To start server and bind to ip"
+	;;
+esac
